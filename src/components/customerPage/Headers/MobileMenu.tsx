c0ba43bbbc7504c3abf4d5c1cdbd043ea75161b9
wittaya <w.imlukin@hotmail.com>
2025-04-10 11:52:37 +0700

    Menu Mobile

--- src/components/customerPage/Headers/MobileMenu.tsx
+++ src/components/customerPage/Headers/MobileMenu.tsx
@@ -4,8 +4,10 @@ import { Divide, Menu } from 'lucide-react'
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetFooter, SheetTrigger } from '@/components/ui/sheet'
 import { UserType } from '@/types/user'
 import { AuthButtons, SignoutButton, UserAvatar } from './UserComp'
-
-
+import { MobileNavLink } from './NaveLink'
+import { Separator } from '@/components/ui/separator'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import Link from 'next/link'
 interface MobileMenuProps {
     user: UserType | null
 }
@@ -17,33 +19,39 @@ const MobileMenu = ({ user }: MobileMenuProps) => {
                     <Menu size={20} />
                 </Button>
             </SheetTrigger>
-
             <SheetContent className='flex flex-col w-full md:max-w-sm' side='left'>
                 <SheetHeader className='text-left '>
                     <SheetTitle className='text-primary text-xl'>
                         {user ? 'โปรไฟล์ของคุณ' : 'ยินดีต้อนรับ'}
                     </SheetTitle>
                 </SheetHeader>
-                <div>
-
+                <div className='flex flex-1 flex-col gap-6'>
                     {/* User Profile && Auth Buttons */}
-                    {user ? <UserAvatar user= {user}/> : <AuthButtons />}
-
-                    {/* Nav Link */}
-
-                    {/* Go to admin page */}
-                    {user && user.role === 'Admin' && <div>Go to admin page Button</div>}
-
-                    {user && 
-                        <SheetFooter>
-                            <SignoutButton />
-                        </SheetFooter>
-                    }
-
+                    {user ? <UserAvatar user={user} /> : <AuthButtons />}
+                    <Separator />
+                    <div className='px-4'>
+                        <ScrollArea className='h-48 sm:h-60 w-full'>
+                            {/* Nav Link */}
+                            <MobileNavLink />
+                            {/* Go to admin page */}
+                            {user && user.role === 'Admin' && (
+                                <div className='mt-4 pb-4'>
+                                    <Separator className='mb-4' />
+                                    <Button variant='secondary' size='lg' className='w-full'>
+                                        <Link href='/amdin'>หลังบ้าน</Link>
+                                    </Button>
+                                </div>
+                            )}
+                        </ScrollArea>
+                    </div>
                 </div>
+                {user &&
+                    (<SheetFooter>
+                        <SignoutButton />
+                    </SheetFooter>
+                    )}
             </SheetContent>
         </Sheet>
     )
 }
-
 export default MobileMenu
---
+++ src/components/customerPage/Headers/NaveLink.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button"
+import { SheetClose } from "@/components/ui/sheet"
+import Link from "next/link"
+import { title } from "process"
+
+const NAV_LINKS = [
+    { title: 'หน้าหลัก', href: '/' },
+    { title: 'สินค้าทั้งหมด', href: '/products' },
+    { title: 'เกี่ยวกับ', href: '/aobut' },
+    { title: 'ติดต่อเรา', href: '/contact' },
+]
+
+export const MobileNavLink = () => (
+    <div className='flex flex-col gap-2 mt-2'>
+        {NAV_LINKS.map((link, index) => (
+            <SheetClose asChild key={index}>
+                <Button asChild variant='secondary' size='lg'>
+                    <Link href={link.href} className="flex items-center gap-2">{link.title}</Link>
+                </Button>
+            </SheetClose>
+        ))}
+    </div>
+
+)
